e1e90ade0401eab001424f61bff99f0831580366
Manas Verma <manas.verma_cs22@gla.ac.in>
2026-02-13 11:27:42 +0530

    Add Java program to sum two numbers

---
+++ src/SumTwoNumbers.java
@@ -0,0 +1,13 @@
+public class SumTwoNumbers {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 20;
+        int result = sum(first, second);
+
+        System.out.println("Sum of " + first + " and " + second + " is: " + result);
+    }
+}
